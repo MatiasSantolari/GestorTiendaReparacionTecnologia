6c30b0016a64241d409e519c82fe7677e18b3c7e
Matias <santolarimatias@gmail.com>
2025-09-25 16:29:30 -0300

    Reestructura de solución: mover .sln, .gitignore, readme

--- Datos/obj/Debug/net8.0/Datos.AssemblyInfo.cs
+++ Datos/obj/Debug/net8.0/Datos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Datos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21101b67ac5ff5ac3136004990c9062f21a20352")]
 [assembly: System.Reflection.AssemblyProductAttribute("Datos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Datos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
